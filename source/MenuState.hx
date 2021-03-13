18d72d7f68b934c5d783a14503dd4175be509194
Matt Swieboda <mswieboda@users.noreply.github.com>
2021-03-13 14:51:05 -0600

    init haxe game

---
+++ source/MenuState.hx
@@ -0,0 +1,25 @@
+package;
+
+import flixel.FlxState;
+import flixel.FlxG;
+import flixel.ui.FlxButton;
+
+class MenuState extends FlxState {
+  var playButton:FlxButton;
+
+  override public function create() {
+    super.create();
+
+    playButton = new FlxButton(0, 0, "Play", clickPlay);
+    playButton.screenCenter();
+    add(playButton);
+  }
+
+  override public function update(elapsed:Float) {
+    super.update(elapsed);
+  }
+
+  function clickPlay() {
+    FlxG.switchState(new PlayState());
+  }
+}
